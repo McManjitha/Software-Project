196f616e784311a4d630567f84da56c8fb30e1ca
GaminduSahan <sahangwg@gmail.com>
2023-06-06 09:07:26 +0530

    added the server file

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require("mongoose");
+
+const app = express();
+
+var waypointList = [];
+var planeList = [];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'secret key',
+  resave: false,
+  saveUninitialized: false,
+}));
