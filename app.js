8965aa9149f40142940f5a57a761624dde800ed0
GaminduSahan <sahangwg@gmail.com>
2023-06-10 08:57:11 +0530

    connected to mongodb

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
 }));
+
+const server = app.listen(3000, () => {
+  console.log('Server started');
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/FlightSimulator',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+).then(() => {
+  console.log('Connected to MongoDB');
+  
+})
+.catch((error) => {
+  console.log('Error connecting to MongoDB', error);
+});
